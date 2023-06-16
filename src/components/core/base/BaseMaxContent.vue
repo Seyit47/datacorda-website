b2ee9b1794cd8f68247ea306af19713ced80f497
Seyit47 <seyitbu1111@gmail.com>
2023-06-16 16:26:14 +0500

    feat: create base max content

---
+++ src/components/core/base/BaseMaxContent.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineOptions({
+    name: "BaseMaxContent",
+});
+</script>
+
+<template>
+    <div class="max-w-[78rem] w-full mx-auto">
+        <slot />
+    </div>
+</template>
