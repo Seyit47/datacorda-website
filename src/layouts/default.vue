142886ad8ec4e77aa17c3ccd2d561d3c58480fd7
Seyit47 <seyitbu1111@gmail.com>
2023-06-24 11:23:57 +0500

    feat(style): layout changes

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,7 +4,7 @@ import AppHeader from "@/components/core/app/AppHeader.vue";
 
 <template>
     <div class="relative min-h-screen w-full">
-        <AppHeader />
+        <AppHeader class="sticky top-0 bg-white z-[9999] shadow-md" />
         <main class="w-full">
             <slot />
         </main>
