78638f99fba1bf82d596f255e2ef4addcb25ea0a
Seyit47 <seyitbu1111@gmail.com>
2023-06-24 11:24:36 +0500

    feat: add aos animations library

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,41 +1,38 @@
 <script lang="ts" setup>
 import BaseMaxContent from "@/components/core/base/BaseMaxContent.vue";
+import MainSection from "@/components/pages/home/MainSection.vue";
+import AudienceSection from "@/components/pages/home/AudienceSection.vue";
 </script>
 
 <template>
-    <div class="w-full bg-black">
-        <BaseMaxContent class="px-6 pt-12 pb-20">
-            <section class="w-full flex items-center">
-                <div class="grid grid-cols-12 items-center">
-                    <div class="col-span-5">
-                        <div class="flex flex-col gap-y-2.5">
-                            <h1 class="text-[4rem] text-white font-bold">Data Science Company</h1>
-                            <div class="flex flex-col gap-y-7.5">
-                                <p class="text-size_18/16 text-white">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                    Temporibus veniam maiores debitis.
+    <div class="relative overflow-hidden w-full pt-12">
+        <MainSection />
+        <AudienceSection />
+        <section id="about">
+            <BaseMaxContent class="px-6 py-6">
+                <div class="min-h-screen flex items-center">
+                    <div class="grid grid-cols-12">
+                        <div class="col-span-6 bg-white shadow-lg">
+                            <div class="flex flex-col gap-y-5 py-6 px-6">
+                                <h2 class="text-5xl font-bold">Who we are</h2>
+                                <p class="text-size_18/16 leading-[1.6]">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
+                                    quidem neque officiis ea id, recusandae dicta eius magni nulla
+                                    cupiditate vitae illo quae tempora, placeat tenetur aspernatur
+                                    mollitia eos nemo.
                                 </p>
-                                <div class="w-55">
+                                <div class="w-52">
                                     <button
-                                        class="flex items-center justify-center w-full h-full py-4 text-white bg-cl-main hover:bg-cl-main/90 rounded-full font-bold transition-colors duration-150"
+                                        class="flex items-center justify-center w-full h-full py-4 border-2 border-cl-main text-white hover:text-cl-text bg-cl-main hover:bg-white rounded-full font-bold transition-colors duration-150"
                                     >
-                                        Get Started
+                                        Sign up
                                     </button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-span-7">
-                        <div class="relative pt-[76.23%]">
-                            <img
-                                src="@/assets/img/banner.svg"
-                                alt=""
-                                class="absolute top-0 left-0 w-full h-full"
-                            />
-                        </div>
-                    </div>
                 </div>
-            </section>
-        </BaseMaxContent>
+            </BaseMaxContent>
+        </section>
     </div>
 </template>
