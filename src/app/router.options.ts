3ddbbb30b8635627972afd561ea40a88ee566b09
Seyit47 <seyitbu1111@gmail.com>
2023-06-24 11:22:29 +0500

    feat: change default router behaviour

---
+++ src/app/router.options.ts
@@ -0,0 +1,32 @@
+import type { RouterConfig } from "@nuxt/schema";
+
+export default <RouterConfig>{
+    async scrollBehavior(to, from, savedPosition) {
+        if (from.name === "index" && to.name === "index" && to.hash) {
+            return {
+                top: 82,
+                el: to.hash,
+                behavior: "smooth",
+            };
+        }
+
+        const app = useNuxtApp();
+
+        await new Promise<void>((resolve) => {
+            const handler = () => {
+                resolve();
+            };
+            app.hook("page:finish", handler);
+        });
+
+        if (to.hash) {
+            return {
+                top: 82,
+                el: to.hash,
+                behavior: "smooth",
+            };
+        }
+
+        return savedPosition || { left: 0, top: 0 };
+    },
+};
