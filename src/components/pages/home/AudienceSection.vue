36853dc3f4bca045a20ffd094ca9f2c47d20dd03
Seyit47 <seyitbu1111@gmail.com>
2023-06-24 11:24:19 +0500

    feat(style): solutions section

---
+++ src/components/pages/home/AudienceSection.vue
@@ -0,0 +1,213 @@
+<script lang="ts" setup>
+import BaseMaxContent from "@/components/core/base/BaseMaxContent.vue";
+</script>
+
+<template>
+    <BaseMaxContent class="px-6">
+        <section id="solutions" ref="wrapperEl" class="flex flex-col gap-y-20 py-6">
+            <div class="grid grid-cols-12 items-center">
+                <div data-aos="fade-up-right" class="col-span-7">
+                    <div class="relative pt-[90.71%]">
+                        <img
+                            src="@/assets/img/why-us.svg"
+                            alt=""
+                            class="main-banner-right-animation absolute top-0 -left-[20%] w-full h-full"
+                        />
+                    </div>
+                </div>
+
+                <div data-aos="fade-up-left" class="col-span-5">
+                    <div class="flex flex-col gap-y-2.5">
+                        <div>
+                            <span
+                                class="py-2.5 px-4 rounded-full bg-cl-main/10 text-cl-main text-size_12/16 tracking-widest uppercase"
+                            >
+                                over 150k+ client
+                            </span>
+                        </div>
+
+                        <h2 class="text-[3.5rem] font-semibold">
+                            Target
+                            <span class="text-cl-main underline">Audience</span> for your business.
+                        </h2>
+
+                        <p class="text-size_18/16 leading-[1.8]">
+                            AI technology is perfect for best business solutions & we offer help to
+                            achieve your goals.
+                        </p>
+
+                        <ul class="mt-6 flex flex-col gap-y-3.75">
+                            <li class="font-medium">Various analysis options.</li>
+                            <li class="font-medium">Page Load (time, size, number of requests).</li>
+                            <li class="font-medium">Advance Data analysis operation.</li>
+                        </ul>
+
+                        <div class="w-55 mt-14">
+                            <button
+                                class="flex items-center justify-center w-full h-full py-4 border-2 border-cl-main text-white hover:text-cl-text bg-cl-main hover:bg-white rounded-full font-bold transition-colors duration-150"
+                            >
+                                More details
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-12 items-center">
+                <div data-aos="fade-up-right" class="col-span-5">
+                    <div class="flex flex-col gap-y-2.5">
+                        <div>
+                            <span
+                                class="py-2.5 px-4 rounded-full bg-cl-main/10 text-cl-main text-size_12/16 tracking-widest uppercase"
+                            >
+                                over 150k+ client
+                            </span>
+                        </div>
+
+                        <h2 class="text-[3.5rem] font-semibold">
+                            Target
+                            <span class="text-cl-main underline">Audience</span> for your business.
+                        </h2>
+
+                        <p class="text-size_18/16 leading-[1.8]">
+                            AI technology is perfect for best business solutions & we offer help to
+                            achieve your goals.
+                        </p>
+
+                        <ul class="mt-6 flex flex-col gap-y-3.75">
+                            <li class="font-medium">Various analysis options.</li>
+                            <li class="font-medium">Page Load (time, size, number of requests).</li>
+                            <li class="font-medium">Advance Data analysis operation.</li>
+                        </ul>
+
+                        <div class="w-55 mt-14">
+                            <button
+                                class="flex items-center justify-center w-full h-full py-4 border-2 border-cl-main text-white hover:text-cl-text bg-cl-main hover:bg-white rounded-full font-bold transition-colors duration-150"
+                            >
+                                More details
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div data-aos="fade-up-left" class="col-span-7">
+                    <div class="relative pt-[90.71%]">
+                        <img
+                            src="@/assets/img/why-us.svg"
+                            alt=""
+                            class="main-banner-right-animation absolute top-0 -right-[20%] w-full h-full"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-12 items-center">
+                <div data-aos="fade-up-right" class="col-span-7">
+                    <div class="relative pt-[90.71%]">
+                        <img
+                            src="@/assets/img/why-us.svg"
+                            alt=""
+                            class="main-banner-right-animation absolute top-0 -left-[20%] w-full h-full"
+                        />
+                    </div>
+                </div>
+
+                <div data-aos="fade-up-left" class="col-span-5">
+                    <div class="flex flex-col gap-y-2.5">
+                        <div>
+                            <span
+                                class="py-2.5 px-4 rounded-full bg-cl-main/10 text-cl-main text-size_12/16 tracking-widest uppercase"
+                            >
+                                over 150k+ client
+                            </span>
+                        </div>
+
+                        <h2 class="text-[3.5rem] font-semibold">
+                            Target
+                            <span class="text-cl-main underline">Audience</span> for your business.
+                        </h2>
+
+                        <p class="text-size_18/16 leading-[1.8]">
+                            AI technology is perfect for best business solutions & we offer help to
+                            achieve your goals.
+                        </p>
+
+                        <ul class="mt-6 flex flex-col gap-y-3.75">
+                            <li class="font-medium">Various analysis options.</li>
+                            <li class="font-medium">Page Load (time, size, number of requests).</li>
+                            <li class="font-medium">Advance Data analysis operation.</li>
+                        </ul>
+
+                        <div class="w-55 mt-14">
+                            <button
+                                class="flex items-center justify-center w-full h-full py-4 border-2 border-cl-main text-white hover:text-cl-text bg-cl-main hover:bg-white rounded-full font-bold transition-colors duration-150"
+                            >
+                                More details
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-12 items-center">
+                <div data-aos="fade-up-right" class="col-span-5">
+                    <div class="flex flex-col gap-y-2.5">
+                        <div>
+                            <span
+                                class="py-2.5 px-4 rounded-full bg-cl-main/10 text-cl-main text-size_12/16 tracking-widest uppercase"
+                            >
+                                over 150k+ client
+                            </span>
+                        </div>
+
+                        <h2 class="text-[3.5rem] font-semibold">
+                            Target
+                            <span class="text-cl-main underline">Audience</span> for your business.
+                        </h2>
+
+                        <p class="text-size_18/16 leading-[1.8]">
+                            AI technology is perfect for best business solutions & we offer help to
+                            achieve your goals.
+                        </p>
+
+                        <ul class="mt-6 flex flex-col gap-y-3.75">
+                            <li class="font-medium">Various analysis options.</li>
+                            <li class="font-medium">Page Load (time, size, number of requests).</li>
+                            <li class="font-medium">Advance Data analysis operation.</li>
+                        </ul>
+
+                        <div class="w-55 mt-14">
+                            <button
+                                class="flex items-center justify-center w-full h-full py-4 border-2 border-cl-main text-white hover:text-cl-text bg-cl-main hover:bg-white rounded-full font-bold transition-colors duration-150"
+                            >
+                                More details
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div data-aos="fade-up-left" class="col-span-7">
+                    <div class="relative pt-[90.71%]">
+                        <img
+                            src="@/assets/img/why-us.svg"
+                            alt=""
+                            class="main-banner-right-animation absolute top-0 -right-[20%] w-full h-full"
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </BaseMaxContent>
+</template>
+
+<style lang="scss" scoped>
+.main-banner-right-animation {
+    animation: main-banner-right 1.5s ease-in-out infinite alternate;
+}
+
+@keyframes main-banner-right {
+    from {
+        transform: translateY(0px);
+    }
+    to {
+        transform: translateY(10px);
+    }
+}
+</style>
