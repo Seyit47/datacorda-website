de9ab5238757e5420b7e0d39460293d94bc6f5fb
Seyit47 <seyitbu1111@gmail.com>
2023-07-03 23:48:50 +0500

    feat: add svg animation

--- src/components/pages/home/MainSection.vue
+++ src/components/pages/home/MainSection.vue
@@ -1,8 +1,42 @@
 <script lang="ts" setup>
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import BaseMaxContent from "@/components/core/base/BaseMaxContent.vue";
 
 defineOptions({
     name: "MainSection",
+});
+
+const circle = ref<SVGCircleElement | null>(null);
+const exampleLine = ref<SVGPathElement | null>(null);
+
+onMounted(() => {
+    gsap.registerPlugin(MotionPathPlugin);
+
+    if (circle.value && exampleLine.value) {
+        const dashes = exampleLine.value.getTotalLength();
+
+        exampleLine.value.style.strokeDasharray = `${dashes}`;
+        exampleLine.value.style.strokeDashoffset = `${dashes}`;
+
+        gsap.to(exampleLine.value, {
+            duration: 5,
+            strokeDashoffset: 1940,
+            repeat: -1,
+        });
+
+        gsap.to(circle.value, {
+            duration: 5,
+            repeat: -1,
+            motionPath: {
+                path: exampleLine.value,
+                align: exampleLine.value,
+                alignOrigin: [0.5, 0.5],
+                start: 0,
+                end: 0.487,
+            },
+        });
+    }
 });
 </script>
 
@@ -10,33 +44,69 @@ defineOptions({
     <div
         class="relative w-full h-full min-h-[calc(100vh-8.125rem)] bg-gradient-to-b from-white to-cl-main/20"
     >
-        <div
-            id="example-line"
-            ref="exampleLine"
-            class="absolute top-0 left-0 w-full h-full scale-x-[-1] flex items-end text-cl-main"
-        >
+        <div class="absolute top-0 right-0 w-full h-full flex items-end text-cl-main">
             <svg
+                id="e1eoxGqLUl21"
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 1440 320"
-                class="absolute stroke-2"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 1920 1080"
+                shape-rendering="geometricPrecision"
+                text-rendering="geometricPrecision"
             >
                 <path
-                    fill="currentColor"
-                    fill-opacity="0.5"
-                    stroke="white"
-                    d="M0,32L30,58.7C60,85,120,139,180,144C240,149,300,107,360,80C420,53,480,43,540,69.3C600,96,660,160,720,181.3C780,203,840,181,900,154.7C960,128,1020,96,1080,117.3C1140,139,1200,213,1260,256C1320,299,1380,309,1410,314.7L1440,320L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
-                ></path>
+                    d="M627.928894,1080l156.215035-96.174522c136.532336-84.943134,269.021034-65.107376,357.077003-40.593145c401.689529,135.859318,510.110003-209.210823,563.246464-254.487778c51.011-71.818655,136.029336-151.443668,252.929543-34.348059c106.272916,67.134817,235.925862-31.225512,278.435028-60.889721c110.523831-87.431398,206.169475-221.701018,310.151618-132.708359v619.826093L627.928894,1080Z"
+                    transform="matrix(.879719 0 0 1.197615-319.750137-213.4242)"
+                    opacity="0.71"
+                    fill="#35275E"
+                />
             </svg>
         </div>
-
-        <div class="absolute top-0 left-0 w-full h-full scale-x-[-1] flex items-end text-cl-main">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="relative">
+        <div class="absolute top-0 left-0 w-full h-full flex items-end text-cl-main">
+            <svg
+                id="e0XKMkTo6es1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 1920 1080"
+                shape-rendering="geometricPrecision"
+                text-rendering="geometricPrecision"
+            >
                 <path
-                    fill="currentColor"
-                    fill-opacity="0.6"
-                    d="M0,32L30,74.7C60,117,120,203,180,245.3C240,288,300,288,360,256C420,224,480,160,540,122.7C600,85,660,75,720,96C780,117,840,171,900,208C960,245,1020,267,1080,256C1140,245,1200,203,1260,202.7C1320,203,1380,245,1410,266.7L1440,288L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
-                ></path>
+                    ref="exampleLine"
+                    d="M627.928894,1080L818.560847,895.257876c114.431002-101.17787,190.881671-50.385854,314.158253-.000001c131.778417,57.342105,279.268696-163.768833,329.446041-228.37118c62.471949-74.351106,138.154792-88.992667,233.800423,21.85786c48.885543,56.205878,80.767417,29.664209,123.276583,0c110.523831-87.431398,167.911213-167.056418,271.893356-78.063759v469.319204h-1463.206609Z"
+                    transform="matrix(.879719 0 0 1.197615 80.388366-213.4242)"
+                    fill="#35275E"
+                    stroke="white"
+                    opacity="0.7"
+                    stroke-width="3.84"
+                />
             </svg>
+            <div class="absolute w-6">
+                <svg
+                    id="eS9u3BoaVbH1"
+                    ref="circle"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 300 300"
+                    shape-rendering="geometricPrecision"
+                    text-rendering="geometricPrecision"
+                >
+                    <ellipse
+                        rx="110"
+                        ry="110"
+                        transform="translate(150.000001 150)"
+                        stroke-width="0"
+                        fill="white"
+                    />
+                    <ellipse
+                        rx="140.561455"
+                        ry="139.935961"
+                        transform="matrix(.960434 0 0 0.964727 150.000001 150)"
+                        fill="none"
+                        stroke="white"
+                        stroke-width="30"
+                    />
+                </svg>
+            </div>
         </div>
 
         <BaseMaxContent class="relative px-6">
@@ -68,25 +138,9 @@ defineOptions({
 </template>
 
 <style lang="scss" scoped>
-.element {
-    background: lightblue;
-    width: 50px;
-    height: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 2px solid slateblue;
-    border-radius: 5px;
-}
-.element {
-    offset-path: path(
-        "M0,32L30,58.7C60,85,120,139,180,144C240,149,300,107,360,80C420,53,480,43,540,69.3C600,96,660,160,720,181.3C780,203,840,181,900,154.7C960,128,1020,96,1080,117.3C1140,139,1200,213,1260,256C1320,299,1380,309,1410,314.7L1440,320L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
-    );
-    animation: move 2000ms infinite ease-in-out;
-}
-@keyframes move {
-    100% {
-        offset-distance: 48%;
+@keyframes dash {
+    to {
+        stroke-dashoffset: 1000;
     }
 }
 
